f93177e2975639770afe388ba0005c7c51d259b2
larakucukdevlett <larakk16@gmail.com>
2025-04-28 17:49:18 +0300

    Core layer added

--- Core/DataAccess/IEntityRepository.cs
+++ Core/DataAccess/IEntityRepository.cs
@@ -1,5 +1,4 @@
 ﻿using Entities.Abstract;
-using Entities.Concrete;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -7,9 +6,9 @@ using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace DAO.Abstract
+namespace Core.DataAcess
 {
-    public interface IEntityRepository<T> where T:class,IEntity  //generic constraint: T bir referans tipi olmalı ve ya IEntity olabilir ya da IEntity den implemente eden bir class olabilir 
+    public interface IEntityRepository<T> where T:class,IEntity,new() //generic constraint: T bir referans tipi olmalı ve ya IEntity olabilir ya da IEntity den implemente eden bir class olabilir 
     {
         //ürünleri, kategorileri listelemek için delegate yapısı oluşturduk.
         List<T> GetAll(Expression<Func<T,bool>>filter=null); //DAO Entities referans yaptı
